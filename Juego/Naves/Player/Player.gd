b2acaba4e3207cc99c23ca134734e4f0678c8a13
walterio27 <walterjmedina@hotmail.com>
2023-10-23 10:54:35 -0300

    Efectos Visuales Disparo

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -12,8 +12,19 @@ var dir_rotacion:int = 0
 
 ## Atributos OnReady
 onready var Canion:Canion = $Canion
+onready var laser:RayoLaser = $LaserBeam2D
+
 
 ## Metodos
+func _unhandled_input(event: InputEvent) -> void:
+	#Disparo Rayo
+	if event.is_action_pressed("disparo_secundario"):
+		laser.set_is_casting(true)
+		
+	if event.is_action_released("disparo_secundario"):
+		laser.set_is_casting(false)
+	
+
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	#Los prints son solo para entender mejor que esta pasando. BORRAR
 	print(rad2deg(rotation))
